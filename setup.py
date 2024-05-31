9648a44170c8d7705dbbf53b721fb12d33d794df
Marcelo Ferreira <marceloferreira@Jaydev-MacBook-Pro.local>
2024-05-31 02:21:17 -0300

    v0.2.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dlqhandler',
-    version="0.2.4",
+    version="0.2.5",
     packages=['dlqhandler', 'dlqhandler.services', 'dlqhandler.dataprovider', 'dlqhandler.tests'],
     install_requires=[
         'boto3',
