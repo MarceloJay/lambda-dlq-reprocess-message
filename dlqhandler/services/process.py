54549bce39a3e3784b5e2e18a34bc7b2063397b7
Marcelo Ferreira <marceloferreira@Jaydev-MacBook-Pro.local>
2024-07-16 23:36:39 -0300

    process event

--- dlqhandler/services/process.py
+++ dlqhandler/services/process.py
@@ -25,41 +25,46 @@ class ProcessMessage:
         self.env = env
 
     def execute(self, event):
-
         logging.info("Executing event")
-
+        
         sqs_queue = SQSQueue(self.dlq_queue, self.region_name)
         messages = sqs_queue.receive_messages_dlq(event)
+        # messages = event.get('Records', [])
         
         qtd_msg_capturadas = len(messages)
         if qtd_msg_capturadas == 0:
-            logging.info("Não existem mensagens para extrair da DlQ orquestrador mensageria")
-            return {'message': 'No messages to process'}
+            logging.info("Não existem mensagens para extrair da DLQ orquestrador mensageria")
+            if event.get('Records') is None:
+                return {'message': 'No messages to process'}
+            else:
+                messages = event.get('Records')
         else:
             logging.info(f"Quantidade de mensagens capturadas: {qtd_msg_capturadas}")
 
-        for body, receipt_handle in messages:
-            message = json.loads(body)
-            logger.info(f"Processing message: {message}")
+        for message in messages:
+            body = json.loads(message.get('body'))
+            attributes = message.get('attributes')
+            logging.info(f"Processing message: {body}")
+            logging.info(f"receipt_handle: {attributes}")
 
-            if not receipt_handle.get(ATTEMPTS_KEY):
-                receipt_handle[ATTEMPTS_KEY] = 0
+            if not attributes.get(ATTEMPTS_KEY):
+                attributes[ATTEMPTS_KEY] = 0
+            logging.info(f"receipt_handle: {attributes}")
 
-            attempts = receipt_handle[ATTEMPTS_KEY]    
+            attempts = attributes[ATTEMPTS_KEY]
+            logging.info(f"processamento_tentativas: {attempts}")
 
-            logger.info(f"processamento_tentativas: {attempts}")
             if attempts > self.max_attempts:
                 self.set_status(message, ERROR_STATUS, ERROR_MESSAGE)
                 self.cloudwatch.count('maximo_reprocessamento_alcancada', attempts)
-                #self.dlq_queue.delete_message_dlq(receipt_handle)
-                logger.error(f"Máximo de retentativas alcançadas: {message}")
-                
+                # self.dlq_queue.delete_message_dlq(receipt_handle)
+                logging.info(f"Máximo de retentativas alcançadas: {message}")
             else:
-                logger.info(f"Mensagem para ser reprocessada: {message}")
-                self.increment_attempts(receipt_handle)
-                self.set_status(receipt_handle, REPROCESSING_STATUS)
+                logging.info(f"Mensagem para ser reprocessada: {message}")
+                self.increment_attempts(attributes)
+                self.set_status(attributes, REPROCESSING_STATUS)
                 self.send_to_aws_sqs(self.env, message)
-                self.count_retry_metric(message)
+                self.count_retry_metric(attempts)
 
             #self.dlq_queue.delete_message_dlq(receipt_handle)
 
@@ -78,5 +83,5 @@ class ProcessMessage:
         send_to_sqs = SendToAwsSqs(env)
         send_to_sqs.send_message_to_queue(json.dumps(messagebody))
 
-    def count_retry_metric(self, message):
-        self.cloudwatch.count("reprocessamento_quantidade", 1)
+    def count_retry_metric(self, attempts):
+        self.cloudwatch.count("reprocessamento_quantidade", attempts)
