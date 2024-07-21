36a86ee38c914eb6d330ebaa841f261c5efd9111
Marcelo Ferreira <marceloferreira@Jaydev-MacBook-Pro.local>
2024-07-21 19:39:59 -0300

    cloudwatch count

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dlqhandler',
-    version="0.7.6",
+    version="0.7.7",
     packages=['dlqhandler', 'dlqhandler.services', 'dlqhandler.dataprovider'],
     install_requires=[
         'boto3',
